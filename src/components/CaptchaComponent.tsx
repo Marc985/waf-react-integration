4911fbd77428a621f80e4a8e024086a7851f9083
Marc985 <hei.marc.3@gmail.com>
2024-12-19 09:58:41 +0300

    fix container

--- src/components/CaptchaComponent.tsx
+++ src/components/CaptchaComponent.tsx
@@ -39,7 +39,7 @@ const CaptchaComponent: React.FC<CaptchaComponentProps> = ({ onCaptchaSuccess })
 
     const container = document.getElementById("my-captcha-container");
 
-    if (window.AwsWafCaptcha) {
+    if (window.AwsWafCaptcha && container) {
       window.AwsWafCaptcha.renderCaptcha(container, {
         apiKey: apiKey,
         onSuccess: onCaptchaSuccess, // Passer la fonction de rappel ici
