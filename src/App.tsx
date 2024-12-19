fd8dfc9d4dad6543b5850f9fb8c9243a67e9c6cf
Marc985 <hei.marc.3@gmail.com>
2024-12-19 08:24:20 +0300

    add content

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
      <div className="text-center text-4xl">
-      <h1>React AWS WAF Captcha Example</h1>
+      <h1>Captcha</h1>
       <p>Click the button below to show the captcha</p>
       <CaptchaComponent/>
      </div>
